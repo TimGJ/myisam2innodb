2d5b92ffb67b32fbdb2988be5b5ee9de16f992a7
Tim Greening-Jackson <tim@greening-jackson.com>
2017-06-25 13:03:05 +0100

    Fixed minor cut and paste error

--- convertmysqldump.py
+++ convertmysqldump.py
@@ -94,7 +94,6 @@ def ProcessFiles(infile, outfile, excluded):
         foundtable = table_re.search(line)
         if foundtable:
             table   = foundtable.groups()[1]
-            isexcluded = database.upper() in excluded
             logging.info('Line {:,}: Found table {:}'.format(
                     n+1, table))
             
